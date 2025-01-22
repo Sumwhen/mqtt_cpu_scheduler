2bcdb59762eda68a708a4d08513b08246b5735e3
Simon Burkhart <sburkhart96@gmail.com>
2025-01-22 12:06:09 +0100

    fixed some dipatcher issues, and a lot of (semi-working) logic

--- dispatcher.py
+++ dispatcher.py
@@ -1,42 +1,95 @@
 #!/usr/bin/env python
 
+import json
 import time
+import random
+import argparse
 import paho.mqtt.client as mqtt
 
 broker = "localhost"
 port = 1883
-topic = "scheduler"
+
+# config
 
 
 
-def on_connect(client, userdata, flags, rc):
+# argparse:
+parser = argparse.ArgumentParser(
+    prog="mqtt random msg dispatcher"
+)
+
+parser.add_argument('-t', '--topic', type=str,
+    nargs='?',
+    help='topic to publish to')
+parser.add_argument('-n', '--nmsg', type=int,
+    nargs='?',
+    help='number of messages to publish')
+
+parser.add_argument('-s', '--sleep', type=int,
+    nargs='?',
+    help='timeout between publishings')
+
+parser.add_argument('-b', '--batch_size', type=int,
+    nargs='?',
+    help='with batchsize provided, msgs are published in b batches of size n')
+
+parser.add_argument('--seed', nargs='?',
+    help='seed to be used')
+
+args = parser.parse_args()
+
+topic = "schedule" if not args.topic else args.topic
+batch_size = 0 if not args.batch_size else args.batch_size
+seed = random.randint(0, 1000) if not args else args.seed
+sleep = 100 if not args.sleep else args.sleep
+n_msg = 5 if not args.nmsg else args.nmsg
+
+
+print(f"SEED {seed}")
+if batch_size:
+    print(f"running {batch_size} batches, {n_msg} each")
+print(f"timeout between {'batches' if batch_size else 'publishings'}: {sleep} s")
+print(f"publishing total {batch_size * n_msg if batch_size else n_msg} messages to '{topic}''")
+
+
+def on_connect(client, userdata, flags, rc, properties):
     if rc == 0:
         client.connected_flag = True
         print("connected to broker")
     else:
         print(f"Failed to connect, return code {rc}")
 
-def on_publish(client, userdata, mid):
+def on_publish(client, userdata, mid, reason_code, properties):
     print(f"Message published with message id {mid}")
 
 
 def create_msg(name, seconds, deadline, id, success):
-    return str({
+    return json.dumps({
         "id": id,
         "deadline": time.time() + (1000 * deadline),
         "success": success,
         "name": name,
         "processing_time": seconds * 1000
     })
+msg_id = 1
+def random_msg():
+    global msg_id
+    name = "random_000"+str(msg_id)
+    deadline = random.random() * 5
+    success = random.random() * 0.98
+    processing_time = min(random.random() * 2, deadline * 0.9)
+
+    msg_id =+ 1
+    return create_msg(name, processing_time, deadline, msg_id - 1, success)
+
 
 
 unacked_publish = set()
 
-
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 client.connected_flag = False
 client.on_connect = on_connect
-client.on_message = on_publish
+client.on_publish = on_publish
 
 
 client.user_data_set(unacked_publish)
@@ -44,22 +97,36 @@ client.user_data_set(unacked_publish)
 print("connecting....")
 
 client.connect(broker, port, 60)
-while not client.connected_flag:
-    time.sleep(1)
-print("connected")
 
 
 client.loop_start()
 
+if not batch_size:
+    msg_infos = []
+    for i in range(n_msg):
+        msg_infos.append(client.publish(topic, random_msg(), qos=1))
+        time.sleep(sleep / 1000)
 
-msg_info = client.publish(topic, create_msg("msg1", 0.52, 30, 1, 0.87), qos=1)
-unacked_publish.add(msg_info.mid)
+    for info in msg_infos:
+        info.wait_for_publish()
 
-while len(unacked_publish):
-    print("publish not ack")
-    time.sleep(1)
+    print(f"published {n_msg} messages")
 
-msg_info.wait_for_publish()
+else:
+    for i in range(b):
+        print(f"BRANCH {i} of {b}")
+        msg_infos = []
+        for n in range(n_msg):
+            msg_infos.append(client.publish(topic, random_msg(), qos=1))
+
+        for i in msg_infos:
+            i.wait_for_publish()
+
+        time.sleep(sleep / 1000)
+
+    print(f"published {n_msg * b} messages")
+
+
 client.disconnect()
 client.loop_stop()
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,75 @@
+use rumqttc::Event::Incoming;
+use std::ffi::c_float;
+use std::borrow::Borrow;
+use rumqttc::{MqttOptions, AsyncClient, Client, QoS, Event, Packet};
+use std::time::Duration;
+use std::thread;
+use rumqttc::Packet::Publish;
+use rumqttc::v5::mqttbytes::v5::Publish as PublishV5;
+use rumqttc::mqttbytes::v4::Publish as PublishV4;
+use threadpool::ThreadPool;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct Task {
+    id: usize,
+    name: String,
+    success: c_float,
+    deadline: usize,
+    processing_time: usize
+}
+
+async fn async_start() {
+    let topic = "schedule";
+    let mut mqttoptions = MqttOptions::new("scheduler-async", "localhost", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
+    client.subscribe(topic, QoS::AtMostOnce).await.unwrap();
+
+
+
+    while let Ok(notification) = eventloop.poll().await {
+        println!("Received = {:?}", notification);
+
+    }
+}
+const TOPIC: &str = "schedule";
+const NUMBER_OF_WORKERS: usize = 5;
+
 fn main() {
-    println!("Hello, world!");
+    // read from config file
+    let pool = ThreadPool::new(NUMBER_OF_WORKERS);
+
+    start()
+}
+
+fn start() {
+    let topic = "schedule";
+    let mut mqttoptions = MqttOptions::new("scheduler-sync", "localhost", 1883);
+    mqttoptions.set_keep_alive(Duration::from_secs(5));
+
+    let (mut client, mut connection) = Client::new(mqttoptions, 10);
+    client.subscribe(topic, QoS::AtMostOnce).unwrap();
+
+
+    // Iterate to poll the eventloop for connection progress
+    for (i, notification) in connection.iter().enumerate() {
+        println!("Notification = {:?}", notification);
+        match notification {
+            Ok(Incoming(Publish(x))) => {
+                println!("Received {:?}", x);
+                let pl = unwrap_payload(x);
+                println!("Payload = {:?}", pl);
+            }
+            Ok(_) => {}
+            Err(e) => {}
+        }
+    }
+}
+
+fn unwrap_payload(p: PublishV4) -> Option<Task> {
+    let pl = p.payload;
+    let json = serde_json::from_slice::<Task>(pl.borrow()).unwrap();
+    Some(json)
 }
